c3251ab05463943aae1c869c89ac581a5a2c0260
anurag soni <soni.anurag2001.as@gmail.com>
2023-08-28 03:39:01 +0530

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # demo
-#include<iostream>
+#include<iostre
---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a =12;
+    int b=30;
+    int sum= a+b;
+    cout<<sum<<endl;
+    return 0;
+}
